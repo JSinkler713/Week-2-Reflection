02f773495635167f9effbd52baa0bc15127a9ef9
walrusmaximus <matthewjfreeland@gmail.com>
2019-09-27 08:42:45 -0700

    adds base website

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    
+    console.log("Beep boop, I'm a robot")
+
+    $('#btn').click(function(){
+        console.log("clicky clicky")
+        // Add your onclick logic here
+    })
+});
